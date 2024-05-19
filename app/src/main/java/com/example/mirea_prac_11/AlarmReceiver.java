2ee40dff70c835631d110bb6b7204a364991d17a
Semyon <semyonkarabanov@gmail.com>
2024-05-19 14:05:27 +0300

    Работа выполнена!

---
+++ app/src/main/java/com/example/mirea_prac_11/AlarmReceiver.java
@@ -0,0 +1,23 @@
+package com.example.mirea_prac_11;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (context, NotificationActivity.CHANNEL_ID_2)
+                .setSmallIcon(R.drawable.utka)
+                .setContentText("This is your scheduled notification.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1,builder.build());
+
+    }
+}
--- app/src/main/java/com/example/mirea_prac_11/NotificationActivity.java
+++ app/src/main/java/com/example/mirea_prac_11/NotificationActivity.java
@@ -1,7 +1,10 @@
 package com.example.mirea_prac_11;
 
+import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class NotificationActivity extends AppCompatActivity {
     private static final String CHANNEL_ID = "example_channel";
+    public static final String CHANNEL_ID_2 = "delayed_channel";
 
 
     @Override
@@ -64,6 +68,43 @@ public class NotificationActivity extends AppCompatActivity {
 
         }
     }
+
+
+    public void createDelayNotification(View view) {
+        createNotificationChannel_2();
+        scheduleNotification(1000);
+
+
+    }
+
+    private void createNotificationChannel_2() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Delayed Notifications";
+            String description = "Channel for delayed example notifications";
+
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID_2,name,importance);
+            channel.setDescription(description);
+
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
+    private void scheduleNotification(long delay) {
+        Intent notificationIntent = new Intent(NotificationActivity.this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast
+                (this,0,notificationIntent,PendingIntent
+                        .FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        long futureMillis = System.currentTimeMillis() + delay;
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP,futureMillis,pendingIntent);
+    }
+
 
 
 }
